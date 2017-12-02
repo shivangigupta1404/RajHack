cac3b6a97b6fc5ef21d60fc6c6ea82f6e6254114
shivangigupta1404 <shivangi.gupta1404@gmail.com>
2017-12-03 04:35:41 +0530

    take image

--- sfg/feeds.py
+++ sfg/feeds.py
@@ -5,13 +5,19 @@ class Block:
   def __init__(self, index, timestamp, data, previous_hash):
     self.index = index
     self.timestamp = timestamp
-    self.data = data
+    self.user = data[0]
+    self.title= data[1]
+    self.name = data[2]
+    self.age  = data[3]
+    self.sex  = data[4]   
+    self.others =data[5]
+    self.image  = data[6]
     self.previous_hash = previous_hash
     self.hash = self.hash_block()
   
   def hash_block(self):
     sha = hasher.sha256()
-    a= str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash)
+    a= str(self.index)+str(self.timestamp)+str(self.user)+ str(self.title)+str(self.name)+str(self.age)+str(self.sex)+str(self.others) + str(self.previous_hash)
     sha.update(a.encode())
     return sha.hexdigest()
 
@@ -25,17 +31,15 @@ class Blockchain:
   def last_block(self):
     return self.chain[-1]
 
-  def get_next_block(self,last_block):
+  def get_next_block(self,last_block,data):
     this_index = last_block.index + 1
     this_timestamp = date.datetime.now()
-    this_data = "Hey! I'm block " + str(this_index)
-    return Block(this_index, this_timestamp, this_data, last_block.hash)
+    return Block(this_index, this_timestamp, data, last_block.hash)
 
   def add_block(self,data):
-    block_to_add=self.get_next_block(self.last_block())
+    block_to_add=self.get_next_block(self.last_block(),data)
     self.chain.append(block_to_add)
     print ("Block #{} has been added to the blockchain!".format(block_to_add.index))
     print ("Hash: {}\n".format(block_to_add.hash))
 
 blockchain= Blockchain()
-
