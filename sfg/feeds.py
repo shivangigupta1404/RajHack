1fb189887e41707e788186a6f151f96d5ed14046
shivangigupta1404 <shivangi.gupta1404@gmail.com>
2017-12-02 16:53:41 +0530

    added blockchain

--- sfg/feeds.py
+++ sfg/feeds.py
@@ -11,38 +11,31 @@ class Block:
   
   def hash_block(self):
     sha = hasher.sha256()
-    sha.update(str(self.index) + 
-               str(self.timestamp) + 
-               str(self.data) + 
-               str(self.previous_hash))
+    sha.update(str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash))
     return sha.hexdigest()
 
-def create_genesis_block():
-  # Manually construct a block with
-  # index zero and arbitrary previous hash
-  return Block(0, date.datetime.now(), "Genesis Block", "0")
 
-def next_block(last_block):
-  this_index = last_block.index + 1
-  this_timestamp = date.datetime.now()
-  this_data = "Hey! I'm block " + str(this_index)
-  this_hash = last_block.hash
-  return Block(this_index, this_timestamp, this_data, this_hash)
+class Blockchain:
+  def __init__(self):
+    chain = [create_genesis_block()]
 
 
-# Create the blockchain and add the genesis block
-blockchain = [create_genesis_block()]
-previous_block = blockchain[0]
+  def create_genesis_block():
+    # Manually construct a block with index zero and arbitrary previous hash
+    return Block(0, date.datetime.now(), "Genesis Block", "0")
 
-# How many blocks should we add to the chain
-# after the genesis block
-num_of_blocks_to_add = 20
+  def last_block():
+    return blockchain[-1]
 
-# Add blocks to the chain
-for i in range(0, num_of_blocks_to_add):
-  block_to_add = next_block(previous_block)
-  blockchain.append(block_to_add)
-  previous_block = block_to_add
-  # Tell everyone about it!
-  print "Block #{} has been added to the blockchain!".format(block_to_add.index)
-  print "Hash: {}\n".format(block_to_add.hash) 
+  def add_block():
+    block_to_add=get_next_block(last_block())
+    blockchain.append(block_to_add)
+    print "Block #{} has been added to the blockchain!".format(block_to_add.index)
+    print "Hash: {}\n".format(block_to_add.hash)
+
+  def get_next_block(last_block):
+    #change implementation
+    this_index = last_block.index + 1
+    this_timestamp = date.datetime.now()
+    this_data = "Hey! I'm block " + str(this_index)
+    return Block(this_index, this_timestamp, this_data, last_block.hash)
