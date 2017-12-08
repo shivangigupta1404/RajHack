56874262d03959f5d74326431b2f9120fc833c1b
shivangigupta1404 <shivangi.gupta1404@gmail.com>
2017-12-08 09:32:45 +0530

    url mapped to root

--- rajhack/urls.py
+++ rajhack/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^sfg/', include('sfg.urls')),
+    url(r'^',include('sfg.urls')),
+    url(r'^sfg/',include('sfg.urls')),
     url(r'^admin/', admin.site.urls),
 ]
